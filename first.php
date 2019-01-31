fbeb18e66d720ce85318df789670423ce981f054
Aarati Kadu <aartiakadu11@gmail.com>
2019-01-31 19:21:18 +0530

    select aani menupage mdhe problems

---
+++ first.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!--<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mess Management System....</title>
+
+
+-->
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="assets/css/materialize.min.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <style>
+        body {
+         background-image:url("assets/images/bg.jpg");
+         height: 100%; 
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        }
+        </style>
+</head>
+  
+ 
+    <!-- Navbar goes here -->
+
+    <!-- Page Layout here -->
+    
+    <div class="card-panel deep-orange lighten-1"><h2>Mess Management System</h2></div>
+            
+    <div class="row ">
+
+      <div class="col s2 ">
+        <!-- Grey navigation panel -->
+        <ul>
+  <li><a href="default.asp">Register New Student</a></li>
+  <li><a href="news.asp">Today's Special Menu</a></li>
+  <li><a href="contact.asp">Food Chart</a></li>
+  <li><a href="about.asp">Response</a></li>
+</ul>
+        
+      </div>
+
+      <div class="col s8">
+
+      <div class="row">
+    <div class="col s10a m4">
+      <div class="card">
+        <div class="card-image">
+          <img src="assets/images/aalu.jpg">
+          <span class="Aarati Niwas Kadu">Card Title</span>
+        </div>
+        <div class="card-content">
+          <p>PRN: 1742201 <br>
+            class : Ty Comp</p>
+        </div>
+        <div class="card-action">
+          <a href="#">Account Details</a>
+        </div>
+      </div>
+      </div>
+      <div class="col s5">
+        <!-- Teal page content  -->
+       <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="assets/images/qrcode.png">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">QR-Code<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">Record of Plaits</a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+      <p>Date:12.12.72<br>
+         Scan Limit</p>
+    </div>
+  </div>
+  </div>
+
+
+         
+    </div>
+  </div>
+      </div>
+
+    </div>
+          
+
+    
+ 
+ 
+  </body>
+</html>
